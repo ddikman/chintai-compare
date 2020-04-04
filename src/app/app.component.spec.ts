73338a7c3575fb2a95ac34f73141c836ed8aed1f
David <ddikman@gmail.com>
2020-04-04 23:41:33 +0900

    Added button to parsee contents

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,21 +1,23 @@
-import { PropertyParserComponent } from './property-parser/property-parser.component';
+import { AddPropertyInputComponent } from './add-property-input/add-property-input.component';
 import { PropertyCardComponent } from './property-card/property-card.component';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { YenPipe } from './yen.pipe';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
         AppComponent,
         PropertyCardComponent,
         YenPipe,
-        PropertyParserComponent
+        AddPropertyInputComponent
       ],
     }).compileComponents();
   }));
@@ -24,18 +26,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'chintai-compare'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('chintai-compare');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to chintai-compare!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property-card/property-card.component';
 import { YenPipe } from './yen.pipe';
+import { AddPropertyInputComponent } from './add-property-input/add-property-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
-    YenPipe
+    YenPipe,
+    AddPropertyInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
