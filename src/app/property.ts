e076084bcb4030971e2ee58b7872403cba483113
David <ddikman@gmail.com>
2020-04-05 10:40:02 +0900

    Added more data

--- src/app/property.ts
+++ src/app/property.ts
@@ -1,15 +1,3 @@
-export class Property {
-  name: string;
-  link: string;
-
-  floorplanUrl: string;
-  featuredImageUrl: string;
-
-  pricing: Pricing;
-
-  constructor() {}
-}
-
 export class Pricing {
   shikinMonthsRent: number;
   reikinMonthsRent: number;
@@ -20,6 +8,19 @@ export class Pricing {
 
   managementFee: number;
 
+  get agencyFee(): number {
+    const tax = 0.1;
+    return (1 + tax) * this.rent;
+  }
+
+  getPerSqm(sqm: number): number {
+    return Math.round(this.monthlyCost / sqm);
+  }
+
+  get movingCost(): number {
+    return this.shikin + this.reikin + this.keyMoney + this.agencyFee;
+  }
+
   get reikin(): number {
     return this.reikinMonthsRent * this.rent;
   }
@@ -28,12 +29,40 @@ export class Pricing {
     return this.shikinMonthsRent * this.rent;
   }
 
+  get monthlyCost(): number {
+    return this.rent + this.managementFee;
+  }
+
   getAverageCost(overMonths: number): number {
-    const monthlyCost = (this.rent + this.managementFee);
-    const tax = 0.1;
-    const agencyFee = this.rent * (1 + tax);
-    const initialCost = this.shikin + this.reikin;
-    const totalCost = agencyFee + initialCost + monthlyCost * overMonths;
+    const totalCost = this.movingCost + this.agencyFee + this.monthlyCost * overMonths;
     return Math.round(totalCost / overMonths);
+  }
+}
+
+export class Property {
+  name: string;
+  link: string;
+
+  floorplanUrl: string;
+  featuredImageUrl: string;
+  size: number;
+
+  pricing: Pricing;
+
+  static getExample(): Property {
+    const property = new Property();
+    property.featuredImageUrl = 'https://img01.suumo.com/front/gazo/fr/bukken/857/100191265857/100191265857_ro.jpg';
+    property.floorplanUrl = 'https://img01.suumo.com/front/gazo/fr/bukken/857/100191265857/100191265857_co.jpg';
+    property.link = 'https://suumo.jp/chintai/jnc_000056745731/?bc=100191265857';
+    property.name = 'シャローム深瀬';
+    property.size = 52.65;
+    const pricing = new Pricing();
+    pricing.keyMoney = 0;
+    pricing.managementFee = 3000;
+    pricing.rent = 160000;
+    pricing.shikinMonthsRent = 1;
+    pricing.reikinMonthsRent = 1;
+    property.pricing = pricing;
+    return property;
   }
 }
